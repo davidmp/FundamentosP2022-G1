714f5280669ee23af1d5195b8de69cc0b086826f
Yessenia1 <84026815+Yessenia1@users.noreply.github.com>
2022-05-01 18:02:16 -0500

    Delete ejercicios10.py

--- Tarea/1/ejercicios10.py
+++
@@ -1,9 +0,0 @@
-#2.10 Seda Juliaca requiere determinar el pago que debe realizar una persona por el total de metros cúbicos de consumo de agua. Realice un diagrama de flujo y pseudocódigo que represente el algoritmo que permita determinar ese pago.
-p:float
-ca:float
-t:float
-ca=float(input("¿cuántos litros de agua consumió? "))
-p=float(input("¿cuanto es el pago por metro cubico de agua? "))
-mc=ca/1000
-t=p*mc
-print(f"El pago que se debe realizar a una persona por el total de metros cubicos de consumo de agua es: {t}")
