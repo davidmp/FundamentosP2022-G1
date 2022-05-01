5f32591c05d0b62a14b95d0a5e4c85b3f84ede89
Yessenia1 <84026815+Yessenia1@users.noreply.github.com>
2022-05-01 18:03:04 -0500

    Delete ejercicios8.py

--- Tarea/1/ejercicios8.py
+++
@@ -1,7 +0,0 @@
-#2.8 Se requiere determinar el tiempo que tarda una persona en llegar de una ciudad a otra en bicicleta, considerando que lleva una velocidad constante. Realice un diagrama de flujo y pseudocódigo que represente el algoritmo para tal fin.
-d:float
-v:float=5
-t:float
-d=float (input("introduzca la distancia: "))
-t=d/v
-print(f"el tiempo que tarda la persona en llegar a su destino es: {t}")
