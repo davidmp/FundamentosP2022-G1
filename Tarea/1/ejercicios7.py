3625d91e2ab544714db536fcef1507222bcad9b9
Yessenia1 <84026815+Yessenia1@users.noreply.github.com>
2022-05-01 18:02:57 -0500

    Delete ejercicios7.py

--- Tarea/1/ejercicios7.py
+++
@@ -1,8 +0,0 @@
-#2.7 Una compañía de autobuses requiere determinar el costo que tendrá el boleto de un viaje, esto basado en los kilómetros por recorrer y el costo es por kilómetro. Realice un diagrama de flujo y pseudocódigo que representen el algoritmo para tal fin.
-ck:float
-c:float
-km:float
-ck=float(input("introduzca el costo por kilómetros: "))
-km=float(input("introduzca cuántos kilómetros tiene su viaje: "))
-c=ck*km
-print(f"el costo del boleto de su viaje es de: {c}")
