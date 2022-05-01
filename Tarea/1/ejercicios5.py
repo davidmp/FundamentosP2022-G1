c1340251a6a6afcd83aa3b82d2478b9d74191925
Yessenia1 <84026815+Yessenia1@users.noreply.github.com>
2022-05-01 18:02:40 -0500

    Delete ejercicios5.py

--- Tarea/1/ejercicios5.py
+++
@@ -1,8 +0,0 @@
-#2.5 Constructoras Juliaca requiere determinar cuánto cobrar por trabajos de pintura. Considere que se cobra por m2 y realice un diagrama de flujo y pseudocódigo que represente el algoritmo que le permita ir generando presupuestos para cada cliente.
-c:float=10
-m2:float
-presupuesto:float
-c=int(input("¿cuanto cobra por m2?"))
-m2=int(input("¿cuantos metros cuadrados tiene su casa?"))
-presupuesto=c*m2
-print(f"presupuesto: {presupuesto}")
