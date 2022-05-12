01dcfb1d368b993a80fe8fb0dd04385a95bece17
davidmp <mamanipari@gmail.com>
2022-05-12 17:55:35 +0000

    ejemplo nombre reverso

--- EstRepetitivas/EjerciciosR.java
+++ EstRepetitivas/EjerciciosR.java
@@ -1,10 +1,11 @@
 package EstRepetitivas;
 
+import java.util.Scanner;
+
 /**
  * EjerciciosR
  */
 public class EjerciciosR {
-
     public static void imprimirWhileN1_20() {
         int numInit=21;
         while (numInit<=20) {
@@ -26,9 +27,32 @@ public class EjerciciosR {
          }
     }
 
+    public static void nombrevertical() {
+        Scanner lt=new Scanner(System.in);
+        System.out.println("Ingrese su nombre:");
+        String nombre=lt.next();
+        for (int i = 0; i < nombre.length(); i++) {
+            System.out.println(nombre.charAt(i));
+        }
+    }
+
+    public static void nombreInverso() {
+        Scanner lt=new Scanner(System.in);
+        System.out.println("Ingrese su nombre:");
+        String nombre=lt.next();
+        for (int i = nombre.length()-1; i >=0; i--) {
+            System.out.println(nombre.charAt(i));
+        }
+    }
+
     public static void main(String[] args) {
-       System.out.println("Imprime While:");  imprimirWhileN1_20();
+       
+       /*System.out.println("Imprime While:");  imprimirWhileN1_20();
        System.out.println("Imprime Do while:"); imprimirDowhileN1_20();
-       System.out.println("Imprime for:"); imprimirForN1_20();
+       System.out.println("Imprime for:"); imprimirForN1_20();*/
+
+       nombrevertical();
+       nombreInverso();
+
     }
 }
