7daaaccf7698484956d88d45ac794174f917281d
davidmp <mamanipari@gmail.com>
2022-05-19 16:43:56 +0000

    ejemplos varios

---
+++ EstRepetitivas/EjerciciosVarios.java
@@ -0,0 +1,20 @@
+package EstRepetitivas;
+
+import java.util.Scanner;
+
+/**
+ * EjerciciosVarios
+ */
+public class EjerciciosVarios {
+    static Scanner leerT=new Scanner(System.in);
+
+    public static int factorialN(int numero) {
+        
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
