41e634f4d86a0286077f57395ca276a7de9aeb32
davidmp <mamanipari@gmail.com>
2022-06-16 12:37:14 -0500

    suma de matrices

--- ProyectoGUI/src/main/java/pe/edu/upeu/arrays/MatricesEjemplos.java
+++ ProyectoGUI/src/main/java/pe/edu/upeu/arrays/MatricesEjemplos.java
@@ -14,14 +14,50 @@ public class MatricesEjemplos {
         System.out.println("cant filas:"+notas.length);//cantidad filas
         System.out.println("cant columnas:"+notas[0].length);//cantidad columnas
 
-        for (int f = 0; f < notas.length; f++) {
+        for (int f = 0; f < notas.length; f++) {//imprime de forma dinamica una matriz
             for (int c = 0; c < notas[0].length; c++) {
                 System.out.print(notas[f][c]+"\t");
             }
             System.out.println("");
         }
     }
+
+    public void matricesX() {
+        int notas[][] ={
+                        {20,18}, 
+                        {16,10}
+                    };
+        notas[1][1]=15;//modificado
+        for (int f = 0; f < notas.length; f++) {//imprime de forma dinamica una matriz
+            for (int c = 0; c < notas[0].length; c++) {
+                System.out.print(notas[f][c]+"\t");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public void sumaMatrices() {
+        int[][] A={{-3 , 0},{2,5}, {8,-7}};
+        int[][] B={{7 , -5},{4,-2}, {1,-4}};
+        int[][] R=new int[A.length][A[0].length];
+        for (int f = 0; f < A.length; f++) {
+            for (int c = 0; c < B[0].length; c++) {
+              R[f][c]=A[f][c]+B[f][c];
+            }
+        }
+        imprimirM(R);
+    }
+    public void imprimirM(int[][] m) {
+        for (int f = 0; f < m.length; f++) {
+            for (int c = 0; c < m[0].length; c++) {
+                System.out.print(m[f][c]+"\t");
+            }
+            System.out.println("");
+        }        
+    }
     public static void main(String[] args) {
-        new MatricesEjemplos().matricesFBasicos();
+        //new MatricesEjemplos().matricesFBasicos();
+        //new MatricesEjemplos().matricesX();
+        new MatricesEjemplos().sumaMatrices();
     }
 }
