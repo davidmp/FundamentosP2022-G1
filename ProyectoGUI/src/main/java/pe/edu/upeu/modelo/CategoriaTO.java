d3244f7c0b07374da1224a84793cc1db2a789920
davidmp <mamanipari@gmail.com>
2022-06-21 09:44:58 -0500

    modelo

---
+++ ProyectoGUI/src/main/java/pe/edu/upeu/modelo/CategoriaTO.java
@@ -0,0 +1,30 @@
+package pe.edu.upeu.modelo;
+
+/**
+ * CategoriaTO
+ */
+public class CategoriaTO {
+   public String idCat;
+   public String nombreCat;
+
+    public CategoriaTO(String idCat, String nombreCat) {
+        this.idCat = idCat;
+        this.nombreCat = nombreCat;
+    }
+
+
+    public String getIdCat() {
+        return idCat;
+    }
+    public void setIdCat(String idCat) {
+        this.idCat = idCat;
+    }
+    public String getNombreCat() {
+        return nombreCat;
+    }
+    public void setNombreCat(String nombreCat) {
+        this.nombreCat = nombreCat;
+    }
+
+
+}
---
+++ ProyectoGUI/src/main/java/pe/edu/upeu/modelo/ModeloTO.java
@@ -0,0 +1,34 @@
+package pe.edu.upeu.modelo;
+
+/**
+ * ModeloTO
+ */
+public class ModeloTO {
+    public String idModelo;
+    public String nombreModelo;
+    
+    public ModeloTO(String idModelo, String nombreModelo) {
+        this.idModelo = idModelo;
+        this.nombreModelo = nombreModelo;
+    }
+
+    public String getIdModelo() {
+        return idModelo;
+    }
+
+    public void setIdModelo(String idModelo) {
+        this.idModelo = idModelo;
+    }
+
+    public String getNombreModelo() {
+        return nombreModelo;
+    }
+
+    public void setNombreModelo(String nombreModelo) {
+        this.nombreModelo = nombreModelo;
+    }
+
+
+    
+    
+}
