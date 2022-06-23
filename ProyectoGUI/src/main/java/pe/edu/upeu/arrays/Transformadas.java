86e0ef75dd48eaaadb6376e778b70c35162acd66
davidmp <mamanipari@gmail.com>
2022-06-23 13:06:00 -0500

    ejemplo 17

--- ProyectoGUI/src/main/java/pe/edu/upeu/arrays/Transformadas.java
+++ ProyectoGUI/src/main/java/pe/edu/upeu/arrays/Transformadas.java
@@ -21,6 +21,22 @@ public class Transformadas {
         imprimirMatriz(matriz);
     }
 
+    public void transformada17() {
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension=cs.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor=cs.nextInt();            
+        String[][] matriz=new String[dimension][dimension];   
+        for (int c = matriz[0].length-1; c >=0 ; c--) {
+            for (int f =0 ; f < matriz.length-((dimension-1)-c); f++) {               
+                matriz[f][c]=String.valueOf(valor);
+                valor++;
+            }
+        }        
+        imprimirMatriz(matriz);
+    }
+
+
     public void imprimirMatriz(String[][] m) {
         for (int f = 0; f < m.length; f++) {
             for (int c = 0; c < m[0].length; c++) {
@@ -35,7 +51,8 @@ public class Transformadas {
     }
 
     public static void main(String[] args) {
-        new Transformadas().transformada01();
+        //new Transformadas().transformada01();
+        new Transformadas().transformada17();
     }
 
 }
