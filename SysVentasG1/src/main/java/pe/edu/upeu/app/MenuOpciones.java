6d66a2b5439ecac7f92b7cb616016f14ffdcf4a3
davidmp <mamanipari@gmail.com>
2022-06-30 13:04:05 -0500

    Producto crear

--- SysVentasG1/src/main/java/pe/edu/upeu/app/MenuOpciones.java
+++ SysVentasG1/src/main/java/pe/edu/upeu/app/MenuOpciones.java
@@ -5,6 +5,7 @@ import java.io.Console;
 import pe.edu.upeu.dao.CategoriDao;
 import pe.edu.upeu.dao.ClienteDao;
 import pe.edu.upeu.dao.ModeloDao;
+import pe.edu.upeu.dao.ProductoDao;
 import pe.edu.upeu.dao.UsuarioDao;
 import pe.edu.upeu.modelo.UsuarioTO;
 import pe.edu.upeu.util.LeerTeclado;
@@ -21,8 +22,7 @@ public class MenuOpciones {
         System.out.println("Ingrese su clave:");
         char[] clavex=cons.readPassword();
         user.setClave(String.valueOf(clavex));
-        if (user.getUsuario().equals("davidmp") && 
-        user.getClave().equals("123456")) {
+        if (new UsuarioDao().loginValidar(user.getUsuario(), user.getClave())) {
             menuPrincial();
         }else{
             System.out.println("Intente Otra vez!");
@@ -33,7 +33,7 @@ public class MenuOpciones {
     public void menuPrincial() {
         System.out.println("********************Bienvenidos al Sistema de Ventas******************");
         String valorX="1=Registrar Usuario\n"+"2=Registrar Categoria\n"+
-                "3=Registrar cliente\n4=Registrar Modelo";
+                "3=Registrar cliente\n4=Registrar Modelo\n5=Crear Producto";
         int opciones=0;
         do {
             switch (opciones) {
@@ -41,6 +41,7 @@ public class MenuOpciones {
                 case 2: new CategoriDao().crearCategoria(); break;
                 case 3: new ClienteDao().crearCliente();break;
                 case 4: new ModeloDao().crearModelo();break;
+                case 5: new ProductoDao().crearProducto();break;
                 default: System.out.println("Opcion erronea!"); break;
             }
             char continuar=leerT.leer("", "Desea continuar? S=si/N=no")
--- SysVentasG1/src/main/java/pe/edu/upeu/dao/ProductoDao.java
+++ SysVentasG1/src/main/java/pe/edu/upeu/dao/ProductoDao.java
@@ -17,7 +17,22 @@ public class ProductoDao extends AppCrud{
    final static String TABLA_MODELO="Modelo.txt";
 
    public void crearProducto() {
-    
+    leerA=new LeerArchivo(TABLA_PRODUCTO);
+    pTo=new ProductoTO();
+    String idPro=generarId(leerA, 0, "P", 1);
+    pTo.setIdProducto(idPro);
+    pTo.setNombreProd(leerT.leer("", "Nombre Producto"));
+    mostrarCategoria();
+    pTo.setIdCat(leerT.leer("", "Seleccione una Categoria"));
+    mostrarModelo();
+    pTo.setIdMod(leerT.leer("", "Seleccione un modelo"));
+    pTo.setPrecioUnid(leerT.leer(0.0, "ingrese el Precio Unitario"));
+    pTo.setUtilidad(leerT.leer(0.0, "Ingrese la utilidad"));
+    pTo.setUnidMed(leerT.leer("", "Unidad Medida"));
+    pTo.setStock(leerT.leer(0, "Ingrese Cantidad"));
+    pTo.setCantMayor(leerT.leer(0, "Ingrese cantidad para descuento"));
+    leerA=new LeerArchivo(TABLA_PRODUCTO);
+    agregarContenido(leerA, pTo);
    }
 
    public void mostrarCategoria() {
@@ -26,6 +41,7 @@ public class ProductoDao extends AppCrud{
         for (int i = 0; i < dataC.length; i++) {
             System.out.print(dataC[i][0]+"="+dataC[i][1]+", ");
         }
+        System.out.println("");
    }
 
    public void mostrarModelo() {
@@ -33,7 +49,8 @@ public class ProductoDao extends AppCrud{
     Object[][] dataC=listarContenido(leerA);
     for (int i = 0; i < dataC.length; i++) {
         System.out.print(dataC[i][0]+"="+dataC[i][1]+", ");
-    }    
+    }   
+    System.out.println(""); 
    }
 
    
